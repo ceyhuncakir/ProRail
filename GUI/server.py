85bec62ce022845f7f6f76c0cd9185b33c41c3ee
ceyhuncakir <ceyhuncakir@live.nl>
2021-11-09 20:10:06 +0100

    gui fix wgsi server fix
    gui fix wgsi server fix

---
+++ GUI/server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from sklearn.metrics import mean_squared_error
+import pickle
+import math
+from dateutil.relativedelta import relativedelta
+
+app = Flask(__name__, template_folder='template')
+
+@app.route('/', methods=['GET', 'POST'])
+def dashboard():
+    prediction = ""
+    minutes = ""
+    if request.method == "POST":
+        model = pickle.load(open('linear_reg.pkl', 'rb'))
+        user_input_feature_1 = request.form.get('size')
+        print(user_input_feature_1, type(user_input_feature_1))
+        user_input_feature_1 = float(user_input_feature_1)
+        prediction = model.predict([[user_input_feature_1]])[0]
+        rt = relativedelta(seconds=math.trunc(prediction))
+
+        minutes = '{:02d}:{:02d}:{:02d}'.format(int(rt.hours), int(rt.minutes), int(rt.seconds))
+    return render_template('dashboard.html', prediction=minutes)
+
+if __name__ == '__main__':
+    app.run(debug=True)
